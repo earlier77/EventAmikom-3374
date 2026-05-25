576c9a6e2c5d568e1ca9623f55937cf8b32f6e39
earlier77 <dzacky7777@gmail.com>
2026-05-25 13:01:05 +0700

    ujian tengah semester

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,8 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
     public function events() {
-    return $this->hasMany(Event::class);
-}
+        return $this->hasMany(Event::class);
+    }
 }
