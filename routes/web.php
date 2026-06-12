11bb42425478b067c14cbdd1f2d7b729a179d737
earlier77 <dzacky7777@gmail.com>
2026-06-12 19:48:01 +0700

    tugas 8

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,25 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\EventController as EventAdminController;
 // use App\Http\Controllers\Admin\EventController as AdminEventController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\TransactionController;
 
 
+Route::get('/login', function () {
+    return redirect()->route('admin.login');
+})->name('login');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login.post');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::middleware(['auth', 'admin'])->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('events', EventController::class);
+        Route::get('transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    });
+});
 // Route::get('/', function(){
 // return view ('beranda');
 // });
